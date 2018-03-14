62693bb7c9852896c53287afbfd01bcea90bd9b0
geekplux <geekplux@qq.com>
2018-03-15 02:00:55 +0800

    Blog updated

--- js/typing.js
+++ js/typing.js
@@ -59,7 +59,20 @@
 			  });
 			  $('#DonateText,#donateBox,#github').removeClass('blur');
 		  },600);
+    });
 
-	  });
+    $('#menu').click(function (event) {
+      var nav = $('#main-nav');
+      nav.toggle('fast');
+    });
+
+    $(window).resize(function () {
+      var viewportWidth = $(window).width();
+      if (viewportWidth > 468) {
+        $('#main-nav').show('fast');
+      } else {
+        $('#main-nav').hide('fast');
+      }
+    });
   });
 })(jQuery)
